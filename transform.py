8df818742a0c5b35e1e382a83a8b1634bd22d6c7
Hannah Siegel <hannah.siegel@aero.org>
2025-08-18 22:40:36 -0700

    added random center shift augmentation

--- transform.py
+++ transform.py
@@ -91,3 +91,41 @@ class HorizontalFlip(object):
         mask = np.fliplr(mask).copy()
 
         return image, mask
+
+
+
+class CenterShift(object):
+    """
+    Randomly shift the center coordinate of the image.
+    This effectively moves the "center" of the image to make spatial variation
+    """
+    
+    def __init__(self, max_shift_fraction=0.2):
+        """
+        Maximum shift as fraction of image size (0.2 = 20% of image size)
+        """
+        self.max_shift_fraction = max_shift_fraction
+    
+    def __call__(self, sample):
+        image, mask = sample
+        
+        # Get image dimensions (H, W, C format expected)
+        height, width = image.shape[:2]
+        
+        # Calculate maximum shift in pixels
+        max_shift_x = int(width * self.max_shift_fraction)
+        max_shift_y = int(height * self.max_shift_fraction)
+        
+        # Random shift amounts
+        shift_x = np.random.randint(-max_shift_x, max_shift_x + 1)
+        shift_y = np.random.randint(-max_shift_y, max_shift_y + 1)
+        
+        # Shift by rolling arrays (pixels shifted off right edge appear on left, bottom -> top etc)
+        shifted_image = np.roll(image, shift_x, axis=1)  # Roll along width
+        shifted_image = np.roll(shifted_image, shift_y, axis=0)  # Roll along height
+        
+        shifted_mask = np.roll(mask, shift_x, axis=1)
+        shifted_mask = np.roll(shifted_mask, shift_y, axis=0)
+        
+        return shifted_image, shifted_mask
+
